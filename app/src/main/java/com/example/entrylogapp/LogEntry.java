8408882e2800ef9b10480f584c373d6d9adb7a3d
josnajohn15 <josnajohn153@gmail.com>
2024-06-29 14:10:27 +0530

    project created

--- app/src/main/java/com/example/entrylogapp/LogEntry.java
+++ app/src/main/java/com/example/entrylogapp/LogEntry.java
@@ -15,9 +15,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LogEntry extends AppCompatActivity {
     EditText ed1,ed2,ed3,ed4;
     AppCompatButton b1,b2;
+    String apiUrl="http://10.0.4.16:3000/api/students";
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +47,43 @@ public class LogEntry extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name=ed1.getText().toString();
+                //value reading
+                String sname=ed1.getText().toString();
                 String adm=ed2.getText().toString();
                 String sys=ed3.getText().toString();
                 String dep=ed4.getText().toString();
-                Toast.makeText(getApplicationContext(),name+" "+adm+" "+sys+" "+dep,Toast.LENGTH_SHORT).show();
+                //JSON object creation
+                JSONObject student=new JSONObject();
+                try {
+                    student.put("name",sname);
+                    student.put("admission_number",adm);
+                    student.put("system_number",sys);
+                    student.put("department",dep);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+                //JSON OBJECT request creation
+                JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(
+                        Request.Method.POST,
+                        apiUrl,
+                        student,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(getApplicationContext(), "added successfully", Toast.LENGTH_SHORT).show();
+                            }
+                        },
+                        //If any error have occured
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(getApplicationContext(), "something went wrong", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                );
+                RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+                requestQueue.add(jsonObjectRequest);
+
 
             }
         });
@@ -56,6 +99,7 @@ public class LogEntry extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
 
     }
 }
